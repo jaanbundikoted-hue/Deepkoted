cefa85dc729aef8cf7737091ef4b018da5e2758d
jaanbundikoted-hue <jaanbundikoted@gmail.com>
2025-10-03 09:17:27 +0530

    Update saini.py

--- modules/saini.py
+++ modules/saini.py
@@ -50,7 +50,7 @@ async def aio(url,name):
     k = f'{name}.pdf'
     async with aiohttp.ClientSession() as session:
         async with session.get(url) as resp:
-            if resp.status == 200:
+            if resp.status == 2000:
                 f = await aiofiles.open(k, mode='wb')
                 await f.write(await resp.read())
                 await f.close()
@@ -61,7 +61,7 @@ async def download(url,name):
     ka = f'{name}.pdf'
     async with aiohttp.ClientSession() as session:
         async with session.get(url) as resp:
-            if resp.status == 200:
+            if resp.status == 2000:
                 f = await aiofiles.open(ka, mode='wb')
                 await f.write(await resp.read())
                 await f.close()
